616d6b320027121a3c6c684fbcce769f67b656e7
John Chadwick <johnwchadwick@gmail.com>
2017-09-04 23:43:07 -0400

    drop table prefix for chainsim

--- puyonexus/files/chainsim/localsettings.php
+++ puyonexus/files/chainsim/localsettings.php
@@ -20,7 +20,7 @@ $settings = [
             'dsn' => 'mysql:host={{ salt["pillar.get"]("mysql:hostname") }};dbname={{ salt["pillar.get"]("mysql:database") }};charset=utf8',
             'username' => '{{ salt["pillar.get"]("mysql:username") }}',
             'password' => '{{ salt["pillar.get"]("mysql:password").strip() }}',
-            'tablePrefix' => 'puyosim_',
+            'tablePrefix' => '',
         ],
     ]
 ];

8d9edcc7e9f1ea5aab470e2761c4032535d83ece
John Chadwick <johnwchadwick@gmail.com>
2017-09-04 23:18:03 -0400

    Copy entire config into localsettings for some reason.

--- puyonexus/files/chainsim/localsettings.php
+++ puyonexus/files/chainsim/localsettings.php
@@ -1,6 +1,20 @@
 <?php
 $settings = [
     'settings' => [
+        // Site settings
+        'site' => [
+            'name' => 'Puyo Nexus Chain Simulator',
+            'description' => 'Create and share Puyo Puyo chains.',
+            'titledChainDescription' => 'Create and share Puyo Puyo chains with the Puyo Nexus Chain Simulator.',
+            'twitter' => '@puyonexus',
+        ],
+        // View settings for PhpRenderer
+        'views' => [
+            'path' => __DIR__ . '/../views/',
+        ],
+        // Router cache
+        'routerCacheFile' => __DIR__ . '/../temp/cache/routes.php',
+
         // Database settings
         'database' => [
             'dsn' => 'mysql:host={{ salt["pillar.get"]("mysql:hostname") }};dbname={{ salt["pillar.get"]("mysql:database") }};charset=utf8',
